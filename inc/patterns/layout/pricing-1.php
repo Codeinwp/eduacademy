c78216f1b732c454f93002f8a86a74e9687f9f27
John Fraskos <jfraskos@gmail.com>
2025-08-26 11:12:02 +0300

    Update pricing-1.php
    fix typo

--- inc/patterns/layout/pricing-1.php
+++ inc/patterns/layout/pricing-1.php
@@ -17,7 +17,7 @@ return array(
 	'categories' => array( 'eduacademy' ),
 	'keywords'   => array( 'pricing' ),
 	'content'    => '
-	<!-- wp:group {"metadata":"name":"pricing 1"},"align":"full","style":{"spacing":{"padding":{"right":"var:preset|spacing|50","left":"var:preset|spacing|50","top":"var:preset|spacing|80","bottom":"var:preset|spacing|80"},"blockGap":"var:preset|spacing|80","margin":{"top":"0","bottom":"0"}}},"backgroundColor":"ti-bg-alt","layout":{"type":"constrained"}} -->
+	<!-- wp:group {"name":"pricing 1"},"align":"full","style":{"spacing":{"padding":{"right":"var:preset|spacing|50","left":"var:preset|spacing|50","top":"var:preset|spacing|80","bottom":"var:preset|spacing|80"},"blockGap":"var:preset|spacing|80","margin":{"top":"0","bottom":"0"}}},"backgroundColor":"ti-bg-alt","layout":{"type":"constrained"}} -->
 <div class="wp-block-group alignfull has-ti-bg-alt-background-color has-background" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--80);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--80);padding-left:var(--wp--preset--spacing--50)"><!-- wp:group {"align":"wide","layout":{"type":"constrained"}} -->
 <div class="wp-block-group alignwide"><!-- wp:heading {"textAlign":"center","level":1} -->
 <h1 class="wp-block-heading has-text-align-center">Choose Your Learning Path</h1>
